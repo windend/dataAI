9b11a19fb0c8fe9d7a789a5e74c63ef000e41307
windend <fengfengwin@yeah.net>
2017-12-27 16:17:57 +0800

    recreate

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from .models import Choice, Question
 
 # Create your views here.
